9fb4d05fc39a50d9f9a5a31a1f2749007ae5986f
Aliyyah Uddin <uddin.aliyyah@outlook.com>
2023-07-12 11:46:19 +0100

    removed client code from server class

--- src/MyServer.java
+++ src/MyServer.java
@@ -1,69 +1,14 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.net.ServerSocket;
-import java.net.Socket;
-
+import java.io.*;
+import java.net.*;
 public class MyServer {
-
-    /**
-     * Initialise a new server. To run the server, call run().
-     */
-    public MyServer() {}
-
-    /**
-     * Runs the server.
-     * @throws IOException
-     */
-    public void run() throws IOException {
-
-        /*** Set up to accept incoming TCP connections ***/
-
-        // Port numbers will be in lecture 6
-        int port = 587;
-
-        // Open the server socket
-        System.out.println("Opening the server socket on port " + port);
-        ServerSocket serverSocket = new ServerSocket(port);
-
-
-        /*** Receive client connection ***/
-
-        // Waits until a client connects
-        System.out.println("Server waiting for client...");
-        Socket clientSocket = serverSocket.accept();
-        System.out.println("Client connected!");
-
-        // Set up readers and writers for convenience
-        BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        Writer writer = new OutputStreamWriter(clientSocket.getOutputStream());
-
-
-
-        /*** Output what the client says ***/
-
-        String msg;
-        while (true) {
-            msg = reader.readLine();
-            if (msg == null) {
-                break;
-            }
-            System.out.println("Client says " + msg);
-        }
-
-        // Close down the connection
-        clientSocket.close();
-    }
-
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws IOException {
-        MyServer server = new MyServer();
-        server.run();
+    public static void main(String[] args){
+        try{
+            ServerSocket serverSocket=new ServerSocket(587);
+            Socket socket=serverSocket.accept();//establishes connection
+            DataInputStream dis=new DataInputStream(socket.getInputStream());
+            String  str=(String)dis.readUTF();
+            System.out.println("message= "+str);
+            serverSocket.close();
+        }catch(Exception e){System.out.println(e);}
     }
 }
-
