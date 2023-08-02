fe81c523f679096ea08cc2d68b59d565276df57d
Aliyyah Uddin <uddin.aliyyah@outlook.com>
2023-08-02 23:31:00 +0100

    code for 5 account large database

--- src/EmailServer.java
+++ src/EmailServer.java
@@ -1,14 +1,11 @@
-import java.net.ServerSocket;
-import java.net.Socket;
-
-public class EmailServer {
+/*public class EmailServer {
     public static void main(String[] args){
         try{
             ServerSocket serverSocket=new ServerSocket(587);
 
             //establishes connection
             Socket socket=serverSocket.accept();
-            System.out.print(umailEmails.containsKey("glenda@umail.com"));
+            System.out.print(.containsKey("glenda@umail.com"));
             System.out.print(umailEmails.containsKey("bethany@umail.com"));
             System.out.print(umailEmails.containsValue("monkey88!"));
             System.out.print(umailEmails.containsValue("shark66!"));
@@ -16,3 +13,4 @@ public class EmailServer {
         }catch(Exception e){System.out.println(e);}
     }
 }
+*/
